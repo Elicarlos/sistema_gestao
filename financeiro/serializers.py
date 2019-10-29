697481f87a552e763f956acef5cce61329ae1e1a
Elicarlos <elicarlosantos_@hotmail.com>
2019-10-29 10:32:06 -0300

    ajuste

--- financeiro/serializers.py
+++ financeiro/serializers.py
@@ -14,9 +14,10 @@ class  Tipo_saidaSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class EntradaSerializer(serializers.HyperlinkedModelSerializer):
+    tipo_entrada = Tipo_entradaSerializer()
     class Meta:
         model = Entrada
-        fields = ['valor', 'descricao', 'data']
+        fields = ['valor', 'descricao', 'data', 'tipo_entrada']
 
 
 class SaidaSerializer(serializers.HyperlinkedModelSerializer):
